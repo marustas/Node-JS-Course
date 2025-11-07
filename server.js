41e081a9fa2e06db4b855bf5aa579af58d297c7d
marustas <marudov.stas@gmail.com>
2025-11-07 21:24:18 +0200

    feat: connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require('./app')
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT;
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION.replace('<DB_PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB_CONNECTION_STRING, {
+}).then(() => console.log('Database connected'))
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
