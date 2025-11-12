17390e7f24557d66c58045acb50fba1768c674c4
marustas <marudov.stas@gmail.com>
2025-11-12 21:29:57 +0200

    feat(tourModel): implement additional fields

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -9,7 +9,23 @@ const tourSchema = new Schema({
     unique: true,
     trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingQuantity: {
     type: Number,
     default: 0,
   },
@@ -17,6 +33,28 @@ const tourSchema = new Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an image cover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 export type Tour = InferSchemaType<typeof tourSchema>;
