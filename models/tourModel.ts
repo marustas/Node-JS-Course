5194b4f6fe7c3412f1fcb4754d95b722eebd0db3
Stanislau Marudau <51534360+marustas@users.noreply.github.com>
2025-11-15 19:23:03 +0200

    Feature/tour models (#4)
    * Marge develop into main
    
    * Feature/mvc architecture (#1)
    
    * feat: enable typescript
    
    * feat: schema validation for env
    
    * gitignore
    
    * Remove .env from repository
    
    * feat: prettier and eslint
    
    * feat(tourModel): implement additional fields
    
    * feat(tourController): advanced filtering based on query params
    
    * feat(tourController): sorting query
    
    * feat: pagination
    
    * feat(tourController): implement aliasing for top 5 cheapest tours
    
    * feat(tourQuery): extract query modifications to a separate module

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+
 import type { InferSchemaType } from 'mongoose';
 
 const tourSchema = new Schema({
@@ -8,7 +9,23 @@ const tourSchema = new Schema({
     unique: true,
     trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingQuantity: {
     type: Number,
     default: 0,
   },
@@ -16,6 +33,28 @@ const tourSchema = new Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an image cover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 export type Tour = InferSchemaType<typeof tourSchema>;
