44b83a84c3d87c9f90dd681187b962ae68691b7f
marustas <marudov.stas@gmail.com>
2025-11-20 23:10:06 +0200

    feat(errorController): distinguish operational errors

--- controllers/errorController.ts
+++ controllers/errorController.ts
@@ -1,21 +1,32 @@
-import type { ErrorRequestHandler } from 'express';
+import type { ErrorRequestHandler, Response } from 'express';
 import { env } from '../envSchema.ts';
+import type { AppError } from '../models/ApiModels.ts';
+
+const sendProductionError = (error: AppError, res: Response) => {
+  const errorStatusCode = error.isOperational ? error.statusCode : 500;
+  const errorMessage = error.isOperational ? error.message : 'Something went very wrong!';
+
+  res.status(errorStatusCode).json({
+    status: 'error',
+    message: errorMessage,
+  });
+};
+
+const sendDevelopmentError = (error: AppError, res: Response) => {
+  res.status(error.statusCode).json({
+    status: 'error',
+    error,
+    message: error.message,
+    stack: error.stack,
+  });
+};
 
 export const errorController: ErrorRequestHandler<null, unknown, null> = (err, req, res) => {
-  const statusCode = err.statusCode || 500;
   const environment = env.ENVIRONMENT;
-  const message = err.message || 'Internal Server Error';
 
   if (environment === 'development') {
-    res.status(statusCode).json({
-      status: 'error',
-      message,
-      stack: err.stack,
-    });
+    sendDevelopmentError(err, res);
   } else {
-    res.status(statusCode).json({
-      status: 'error',
-      message,
-    });
+    sendProductionError(err, res);
   }
 };
--- models/ApiModels.ts
+++ models/ApiModels.ts
@@ -7,6 +7,7 @@ export interface ResponsePayload<T> {
 
 export class AppError extends Error {
   statusCode: number;
+  isOperational = true;
 
   constructor(message: string, statusCode = 500) {
     super(message);
