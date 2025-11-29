b9f630c52b92c312a3a2f7e9f87efb161a74ed97
marustas <marudov.stas@gmail.com>
2025-11-29 12:30:03 +0200

    feat(authController): rate limiting via express-rate-limit

--- app.ts
+++ app.ts
@@ -2,8 +2,17 @@ import express from 'express';
 import tourRouter from './routes/tourRouter.ts';
 import { errorController } from './controllers/errorController.ts';
 import authRouter from './routes/authRouter.ts';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
+
+const limiter = rateLimit({
+  max: 100, // 100 requests per window time
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again later.',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use('/api/tours', tourRouter);
