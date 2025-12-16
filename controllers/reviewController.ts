a880450db482c8c576dd9529c1ef6df3876e21aa
marustas <marudov.stas@gmail.com>
2025-12-16 21:35:35 +0200

    feat(reviewController): validate input for update endpoint

--- controllers/reviewController.ts
+++ controllers/reviewController.ts
@@ -46,7 +46,39 @@ const createReview: RequestHandler<
   });
 };
 
+const updateReview: RequestHandler<
+  { id: string },
+  ResponsePayload<Review>,
+  Partial<Pick<Review, 'rating' | 'review'>>,
+  null
+> = async (req, res, next) => {
+  const reviewToUpdate = await ReviewModel.findById(req.params.id);
+
+  if (!reviewToUpdate) {
+    return next(new AppError('Review not found', 404));
+  }
+
+  if (reviewToUpdate.user !== req.user._id) {
+    return next(new AppError('You do not have permission to update this review', 403));
+  }
+
+  const updatedReview = await ReviewModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!updatedReview) {
+    return next(new AppError('Failed to update review', 400));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: updatedReview,
+  });
+};
+
 export const reviewController = {
   createReview: catchAsync(createReview),
   getAllReviews: catchAsync(getAllReviews),
+  updateReview: catchAsync(updateReview),
 };
--- routes/reviewRouter.ts
+++ routes/reviewRouter.ts
@@ -3,9 +3,25 @@ import { Router } from 'express';
 import { UserRole } from '../models/userModel.ts';
 import { authController } from '../controllers/authController.ts';
 import { reviewController } from '../controllers/reviewController.ts';
+import { z } from 'zod';
+import { validateBody } from '../utils/validateRequestBody.ts';
+
+const updateReviewRequestBodySchema = z
+  .object({
+    review: z.string(),
+    rating: z.number().min(1).max(5),
+  })
+  .partial()
+  .strict();
 
 export const reviewRouter = Router({ mergeParams: true }); // required to get access to params from parent router
 
 reviewRouter.use(authController.protect, authController.restrictTo(UserRole.USER, UserRole.ADMIN));
 
 reviewRouter.route('/').get(reviewController.getAllReviews).post(reviewController.createReview);
+
+reviewRouter.patch(
+  '/:id',
+  validateBody(updateReviewRequestBodySchema),
+  reviewController.updateReview
+);
